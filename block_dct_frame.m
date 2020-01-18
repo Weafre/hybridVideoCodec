61eb7ee2c8b4af147b2da38eaf19670e61648d45
weafre <doandat.nguyen@gmail.com>
2020-01-19 05:04:35 +0700

    -a

---
+++ block_dct_frame.m
@@ -0,0 +1,16 @@
+function f=block_dct_frame(x,blocksize)
+
+% need to be modified to handle special cases, image size
+% we could use f = blkproc(x, [blockSize blockSize], @dct2);
+
+
+
+f=zeros(size(x));
+
+for i=1:blocksize:size(x,1)-blocksize+1
+        for j=1:blocksize:size(x,2)-blocksize+1
+        f(i:i+blocksize-1,j:j+blocksize-1)=dct2(x(i:i+blocksize-1,j:j+blocksize-1));
+        
+        end
+    
+end
